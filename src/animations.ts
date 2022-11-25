df0f520fb07fd7c93d2a3d38dd9ebc0190437a8b
Ray Cai Flanagan <hjkareus@gmail.com>
2022-11-25 10:43:32 -0500

    rest

--- src/animations.ts
+++ src/animations.ts
@@ -2,25 +2,134 @@ import gsap from 'gsap';
 
 import { getWidth } from './helpers';
 
-export function pageLoadLeave(data: any, done: any) {
-    const page = data.next.url.hash;
+export function navEnter() {
+    const nav = document.querySelector('nav');
+    const openNavButton = document.getElementById('openNavButton');
+    const navContentContainer = document.getElementById('navContentContainer');
 
+    const navEnterTl = gsap.timeline();
+
+    navEnterTl.to(openNavButton, {
+        autoAlpha: 0,
+        x: -200,
+        duration: 0.6,
+        ease: 'power4',
+        onComplete: () => {
+            openNavButton.classList.add('hidden');
+            nav.classList.remove('hidden');
+        }
+    });
+
+    navEnterTl.from(nav, {
+        autoAlpha: 0,
+        x: -getWidth() * .3,
+        duration: 0.6,
+        ease: 'power4',
+        onComplete: () => {
+            let navButtonColor = openNavButton.style.color;
+
+            openNavButton.removeAttribute('style');
+            openNavButton.style.color = navButtonColor;
+
+            let navBackground = nav.style.background;
+
+            nav.removeAttribute('style');
+            nav.style.background = navBackground;
+
+            navContentContainer.classList.remove('hidden');
+        }
+    });
+
+    navEnterTl.from(navContentContainer, {
+        autoAlpha: 0,
+        ease: 'power4',
+        duration: 0.5,
+        clearProps: 'all'
+    });
+}
+
+export function navLeave() {
+    const nav = document.querySelector('nav');
+    const openNavButton = document.getElementById('openNavButton');
+    const navContentContainer = document.getElementById('navContentContainer');
+
+    const navLeaveTl = gsap.timeline();
+
+    navLeaveTl.to(navContentContainer, {
+        autoAlpha: 0,
+        ease: 'power4',
+        duration: 0.5,
+        clearProps: 'all',
+        onComplete: () => navContentContainer.classList.add('hidden')
+    });
+
+    navLeaveTl.to(nav, {
+        autoAlpha: 0,
+        x: -getWidth() * .3,
+        duration: 0.6,
+        ease: 'power4',
+        onComplete: () => {
+            nav.classList.add('hidden');
+            
+            let navBackground = nav.style.background;
+
+            nav.removeAttribute('style');
+            nav.style.background = navBackground;
+
+            openNavButton.classList.remove('hidden');
+        }
+    });
+
+    navLeaveTl.from(openNavButton, {
+        autoAlpha: 0,
+        x: -200,
+        duration: 0.6,
+        ease: 'power4',
+        onComplete: () => {
+            let navButtonColor = openNavButton.style.color;
+
+            openNavButton.removeAttribute('style');
+            openNavButton.style.color = navButtonColor;
+        }
+    });
+}
+
+export function pageLoadLeave(data: any, done: any) {
     const betweenPageLoadContainer = document.querySelector('.between-page-container-black');
     const pageLoadContainer = document.querySelector('.page-load-container');
-    const containers = Array.from(document.querySelectorAll('.container'));
-
+    const nav = document.querySelector('nav');
+    const closeNavButton = document.getElementById('openNavButton');
+    
     pageLoadContainer.classList.add('hidden');
 
     const pageLoadLeaveTl = gsap.timeline();
 
     betweenPageLoadContainer.classList.remove('hidden');
 
+    if(nav.classList.contains('hiden')) pageLoadLeaveTl.to(closeNavButton, {
+        x: -200,
+        duration: 0.5,
+        clearProps: 'all',
+        ease: 'power4',
+        onComplete: () => closeNavButton.classList.add('hidden')
+    });
+    else pageLoadLeaveTl.to(nav, {
+        x: -getWidth(),
+        clearProps: 'all',
+        onComplete: () => nav.classList.add('hidden')
+    });
+
+
     pageLoadLeaveTl.from(betweenPageLoadContainer, {
         x: -getWidth(),
         duration: 1,
         ease: 'power4',
         clearProps: 'all',
-        onComplete: () => done()
+        onComplete: () => {
+            closeNavButton.classList.remove('hidden')
+
+            done();
+        }
     });
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import gsap from 'gsap';
 import barba from '@barba/core';
-import { pageLoadLeave, pageLoadEnter } from './animations';
+import { pageLoadLeave, pageLoadEnter, navEnter, navLeave } from './animations';
 
-function loadDomMethods() {
-    const NUM_PARTICLES = 250;
+const PageStyles = {
+    home: 'light',
+    about: 'dark',
+}
 
+function loadSnow() {
     const snowContainer = document.querySelector('.snow-container');
     
-    for(let i = 0; i < NUM_PARTICLES; i++) {
+    for(let i = 0; i < 200; i++) {
         let snowParticle = document.createElement('div');
     
         snowParticle.classList.add('snow');
@@ -16,20 +19,66 @@ function loadDomMethods() {
     }
 }
 
+function loadDomMethods() {
+    let closeNavButton: HTMLElement = document.querySelector('nav i');
+    let openNavButton = document.getElementById('openNavButton');
+
+    openNavButton.addEventListener('click', () => {
+        navEnter();
+    });
+
+    closeNavButton.addEventListener('click', () => {
+        navLeave();
+    });
+}
+
+function loadDomElements(manualStyle: string | null = null) {
+    const page = (window.location.hash ? window.location.hash : 'home') as keyof typeof PageStyles;
+    const style = manualStyle ? manualStyle : PageStyles[page];
+
+    let closeNavButton: HTMLElement = document.querySelector('nav i');
+    let openNavButton = document.getElementById('openNavButton');
+
+    function changeColors(a: HTMLElement) {
+        a.style.color = style === 'light' ? 'var(--black)' : 'var(--white)'
+    }
+
+    changeColors(openNavButton);
+    closeNavButton.style.color = style !== 'light' ? 'var(--black)' : 'var(--white)'
+
+    document.querySelectorAll('footer a').forEach(
+        (a: HTMLAnchorElement) => changeColors(a)
+    );
+
+    document.querySelectorAll('nav a').forEach(
+        (a: HTMLAnchorElement) => a.style.color = style !== 'light' ? 'var(--black)' : 'var(--white)'
+    );
+
+    document.querySelector('nav').style.background = style === 'light' ? 'var(--black)' : 'var(--white)';
+}
+
 barba.init({
     transitions: [
         {
             once() {
+                loadSnow();
+                loadDomElements('light');
                 loadDomMethods();
+
+                const containers = Array.from(document.querySelectorAll('.container'));
+
+                containers.forEach(
+                    c => c.classList.add('hidden')
+                );
 
                 const pageLoadContainer = document.querySelector('.page-load-container');
                 const betweenPageContainer = document.querySelector('.between-page-container-black');
-                const landingContainer = document.querySelector('.landing');
+                const landingContainer: HTMLDivElement = document.querySelector('.landing');
 
                 betweenPageContainer.classList.add('hidden');
                 landingContainer.classList.add('hidden');
 
-                (landingContainer as any).style.opacity = 0;
+                landingContainer.style.opacity = '0';
 
                 const pageLoadEnterTl = gsap.timeline();
 
@@ -50,6 +99,7 @@ barba.init({
                     onComplete: () => {
                         landingContainer.classList.remove('hidden')
                         pageLoadContainer.classList.add('hidden')
+                        document.querySelector('.landing-container').classList.remove('hidden')
                     }
                 });
 
@@ -69,6 +119,8 @@ barba.init({
                 pageLoadLeave(data, done);
             },
             after(data) {
+                loadDomElements();
+                
                 pageLoadEnter(data);
             }
         }
